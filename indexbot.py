2387c7f54a533725ee012a09ebb88bdc30901b87
John Evans <lgastako@gmail.com>
2023-03-13 00:41:43 -0400

    Initial index bot working.

---
+++ indexbot.py
@@ -0,0 +1,75 @@
+import fire
+import json
+import requests
+import uuid
+
+from llama_index import Document, GPTSimpleVectorIndex
+
+class CLI:
+    def ai(self):
+        from langchain.agents import load_tools
+        from langchain.agents import initialize_agent
+        from langchain.agents import Tool
+        from langchain.llms import OpenAI
+
+        # set OPENAI_API_KEY in your environment
+        llm = OpenAI(temperature=0)
+
+        index_path = "./index_simple.json"
+
+        try:
+            index = GPTSimpleVectorIndex.load_from_disk(index_path)
+        except FileNotFoundError:
+            index = GPTSimpleVectorIndex(documents=[])
+            index.save_to_disk(index_path)
+
+        def doc_from_bytes(contents):
+            return Document(str(contents),  # for now
+                            doc_id=str(uuid.uuid4()))
+
+        def index_file(filename):
+            try:
+                contents = open(filename).read()
+                doc = doc_from_bytes(contents)
+                index.insert(doc)
+                return f"The file {filename} was indexed successfully."
+            except Exception as e:
+                return f"The file {filename} could not be indexed due to an error: {e}"
+
+        def index_url(url):
+            try:
+                contents = requests.get(url).content
+                doc = doc_from_bytes(contents)
+                index.insert(doc)
+                return f"The URL {url} was indexed successfully."
+            except Exception as e:
+                return f"The URL {url} could not be indexed due to an error: {e}"
+
+        def search_index(query):
+            return str(index.query(query))
+
+        tools = [Tool(name="index_file",
+                      description="Add a file to the index.",
+                      func=index_file),
+                 Tool(name="index_url",
+                      description="Add a URL to the index.",
+                      func=index_url),
+                 Tool(name="search_index",
+                      description="Search the index using full english sentences.",
+                      func=search_index)
+                 ]
+        agent = initialize_agent(tools,
+                                 llm,
+                                 agent="zero-shot-react-description",
+                                 verbose=True)
+
+        while True:
+            msg = "Enter a command like 'index the file a /tmp/foo.txt', 'index the URL <url>', or 'search for <query>' or 'exit' to quit.\n> "
+            prompt = input(f"{msg} ")
+            if prompt == "exit":
+                break
+            agent.run(prompt)
+
+
+if __name__ == '__main__':
+    fire.Fire(CLI)
